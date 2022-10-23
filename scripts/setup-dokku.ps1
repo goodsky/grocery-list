18c730bd52e6ad404506b5ca078f3b4f506f6224
Skyler <skylergoodell@outlook.com>
2022-10-23 06:42:11 -0700

    Tweak dokku setup script.

--- scripts/setup-dokku.ps1
+++ scripts/setup-dokku.ps1
@@ -45,7 +45,7 @@ function Download-PublicKey {
 
     $sshkey = $sshkey | ConvertFrom-Json
     $publicKey = $sshKey.publicKey
-    Set-Content -Path $file -Value $publicKey
+    Set-Content -Path $file -Value $publicKey -Force
 }
 
 function Download-PrivateKey {
@@ -74,11 +74,24 @@ if (-not (Test-CommandExists az)) {
 }
 
 # Download public and private keys from Azure
+$certDir = "$env:USERPROFILE\.ssh"
+if (-not (Test-Path $certDir)) {
+    Write-Host "Creating $certDir"
+    New-Item -ItemType Directory -Path $certDir | Out-Null
+}
+
 Write-Host "Downloading Dokku public key from Azure SSH"
-Download-PublicKey -name "skylers-dokku" -file "$env:USERPROFILE\.ssh\dokku_rsa.pub"
+Download-PublicKey -name "skylers-dokku" -file "$certDir\dokku_rsa.pub"
 
 Write-Host "Downloading Dokku private key from Azure KeyVault"
-Download-PrivateKey -name "skylers-dokku-private" -file "$env:USERPROFILE\.ssh\dokku_rsa"
+Download-PrivateKey -name "skylers-dokku-private" -file "$certDir\dokku_rsa"
+
+if (-not (Test-CommandExists ssh-add)) {
+    Write-Error "Could not find OpenSSH commands installed on this computer. Make sure OpenSSH is installed and the service is running."
+    return -1
+}
+
+ssh-add "$certDir\dokku_rsa"
 
 if (-not (Test-CommandExists git)) {
     Write-Error "git is not installed or is not in path"
